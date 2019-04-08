5d0606d58594bf34824ab9f4709165c04f84993c
Goloviatinski Sergiy <sergiy.goloviat@he-arc.ch>
2019-04-08 14:50:18 +0200

    improved image generation algorithm

--- Traumerei/Traumerei/Algorithme/ImageGenerator_RandomFunctions.cs
+++ Traumerei/Traumerei/Algorithme/ImageGenerator_RandomFunctions.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Traumerei.Algorithme
 {
@@ -12,24 +13,57 @@ namespace Traumerei.Algorithme
         private List<Func<Func<double, double, double>, double, double, double>> funcListG;
         private List<Func<Func<double, double, double>, double, double, double>> funcListB;
 
+        private List<Func<double, double, double>> atomicFuncListR;
+        private List<Func<double, double, double>> atomicFuncListG;
+        private List<Func<double, double, double>> atomicFuncListB;
+
+        private List<Combination> combinationListR;
+        private List<Combination> combinationListG;
+        private List<Combination> combinationListB;
+
         private int maxDepth;
 
         private List<Func<Func<double, double, double>, double, double, double>> avalaibleFuncs;
-
         private List<Func<double, double, double>> avalaibleAtomicFuncs;
+
+        private Func<Double, Double> transformX;
+        private Func<Double, Double> transformY;
+
+        enum Combination { ReplaceX, ReplaceY, Imbricate };
 
         public ImageGenerator_RandomFunctions() : base()
         {
-            maxDepth = 3;
 
             funcListR = new List<Func<Func<double, double, double>, double, double, double>>();
             funcListG = new List<Func<Func<double, double, double>, double, double, double>>();
             funcListB = new List<Func<Func<double, double, double>, double, double, double>>();
 
-            avalaibleFuncs = new List<Func<Func<double, double, double>,double, double,double>>()
+            atomicFuncListR = new List<Func<double, double, double>>();
+            atomicFuncListG = new List<Func<double, double, double>>();
+            atomicFuncListB = new List<Func<double, double, double>>();
+
+            combinationListR = new List<Combination>();
+            combinationListG = new List<Combination>();
+            combinationListB = new List<Combination>();
+
+            avalaibleFuncs = new List<Func<Func<double, double, double>, double, double, double>>()
             {
                 (f,x,y)=>Math.Sin(Math.PI*f(x,y)),
-                (f,x,y)=>Math.Cos(Math.PI*f(x,y))
+                (f,x,y)=>Math.Cos(Math.PI*f(x,y)),
+                (f,x,y)=>x*f(x,y),
+                (f,x,y)=>y*f(x,y),
+                (f,x,y)=>x/f(x,y),
+                (f,x,y)=>y/f(x,y),
+                (f,x,y)=>x+f(x,y),
+                (f,x,y)=>y+f(x,y),
+                (f,x,y)=>x-f(x,y),
+                (f,x,y)=>y-f(x,y),
+                (f,x,y)=>f(x,y),
+                (f,x,y)=>Math.Abs(f(x,y)),
+                (f,x,y)=>Math.Cosh(Math.PI*f(x,y)),
+                (f,x,y)=>Math.Sinh(Math.PI*f(x,y)),
+                (f,x,y)=>Math.Log(f(x,y)),
+                (f,x,y)=>Math.Exp(f(x,y)),
             };
 
             avalaibleAtomicFuncs = new List<Func<double, double, double>>()
@@ -38,21 +72,61 @@ namespace Traumerei.Algorithme
                 (x,y)=>y,
                 (x,y)=>x*y,
                 (x,y)=>x/y,
-                (x,y)=>y/x
+                (x,y)=>y/x,
+                (x,y)=>x+y,
+                (x,y)=>x-y,
+                (x,y)=>y-x,
+                (x,y)=>Math.Sin(Math.PI*x),
+                (x,y)=>Math.Sin(Math.PI*y),
+                (x,y)=>Math.Cos(Math.PI*x),
+                (x,y)=>Math.Cos(Math.PI*y),
+                (x,y)=>Math.Sinh(Math.PI*x),
+                (x,y)=>Math.Sinh(Math.PI*y),
+                (x,y)=>Math.Cosh(Math.PI*x),
+                (x,y)=>Math.Cosh(Math.PI*y),
+                (x,y)=>Math.Exp(x),
+                (x,y)=>Math.Exp(y),
+                (x,y)=>Math.Log(x),
+                (x,y)=>Math.Log(y),
             };
+
+            //Adapt "image" coordinate to "mathematic" coordinate
+            transformX = (x => (x - Width / 2) / (Width / 2));
+            transformY = (y => (-y + Height / 2) / (Height / 2));
+
         }
 
-        private double applyFuncsFromList(double x, double y,Func<double,double,double> af, List<Func<Func<double, double, double>, double, double, double>> funcList)
+        private double applyFuncsFromList(double x, double y,
+            List<Func<double, double, double>> atomicFuncList,
+            List<Func<Func<double, double, double>, double, double, double>> funcList,
+            List<Combination> combList)
         {
             double z = 1;
+            double localX = x;
+            double localY = y;
 
-            foreach (Func<Func<double, double, double>, double, double, double> f in funcList)
+            for (int i = 0; i < funcList.Count; i++)
             {
-                z *= f(af, x, y);
+                Func<Func<double, double, double>, double, double, double> f = funcList[i];
+                Func<double, double, double> af = atomicFuncList[i];
+                Combination comb = combList[i];
+                switch (comb)
+                {
+                    case Combination.Imbricate:
+                        break;
+                    case Combination.ReplaceX:
+                        localX = f(af, localX, localY);
+                        break;
+                    case Combination.ReplaceY:
+                        localY = f(af, localX, localY);
+                        break;
+                }
+                z *= f(af, localX, localY);
             }
 
             return z;
         }
+
 
         public new SKBitmap Generate()
         {
@@ -60,24 +134,44 @@ namespace Traumerei.Algorithme
             funcListG.Clear();
             funcListB.Clear();
 
+            atomicFuncListR.Clear();
+            atomicFuncListG.Clear();
+            atomicFuncListB.Clear();
+
+            combinationListR.Clear();
+            combinationListG.Clear();
+            combinationListB.Clear();
+
+            _random = new Random();
+
+            Array combinationValues = Enum.GetValues(typeof(Combination));
+
+            maxDepth = _random.Next(5) + 3;
+
             for (int i = 0; i < _random.Next(maxDepth); i++)
             {
                 funcListR.Add(avalaibleFuncs[_random.Next(avalaibleFuncs.Count)]);
+                atomicFuncListR.Add(avalaibleAtomicFuncs[_random.Next(avalaibleAtomicFuncs.Count)]);
+                combinationListR.Add((Combination)combinationValues.GetValue(_random.Next(combinationValues.Length)));
             }
+
+            maxDepth = _random.Next(5) + 3;
 
             for (int i = 0; i < _random.Next(maxDepth); i++)
             {
                 funcListG.Add(avalaibleFuncs[_random.Next(avalaibleFuncs.Count)]);
+                atomicFuncListG.Add(avalaibleAtomicFuncs[_random.Next(avalaibleAtomicFuncs.Count)]);
+                combinationListG.Add((Combination)combinationValues.GetValue(_random.Next(combinationValues.Length)));
             }
+
+            maxDepth = _random.Next(5) + 3;
 
             for (int i = 0; i < _random.Next(maxDepth); i++)
             {
                 funcListB.Add(avalaibleFuncs[_random.Next(avalaibleFuncs.Count)]);
+                atomicFuncListB.Add(avalaibleAtomicFuncs[_random.Next(avalaibleAtomicFuncs.Count)]);
+                combinationListB.Add((Combination)combinationValues.GetValue(_random.Next(combinationValues.Length)));
             }
-
-            Func<double, double, double> afR = avalaibleAtomicFuncs[_random.Next(avalaibleAtomicFuncs.Count)];
-            Func<double, double, double> afG = avalaibleAtomicFuncs[_random.Next(avalaibleAtomicFuncs.Count)];
-            Func<double, double, double> afB = avalaibleAtomicFuncs[_random.Next(avalaibleAtomicFuncs.Count)];
 
             SKColor color = new SKColor(0, 0, 0);
             double[] RGBvalues = { 0, 0, 0 };
@@ -86,10 +180,9 @@ namespace Traumerei.Algorithme
             {
                 for (int y = 0; y < Height; y++)
                 {
-                    RGBvalues[0] = applyFuncsFromList(x, y,afR, funcListR);
-                    RGBvalues[1] = applyFuncsFromList(x, y,afG, funcListG);
-                    RGBvalues[2] = applyFuncsFromList(x, y,afB, funcListB);
-
+                    RGBvalues[0] = Width / 2 * applyFuncsFromList(transformX(x), transformY(y), atomicFuncListR, funcListR, combinationListR);
+                    RGBvalues[1] = Width / 2 * applyFuncsFromList(transformX(x), transformY(y), atomicFuncListG, funcListG, combinationListG);
+                    RGBvalues[2] = Width / 2 * applyFuncsFromList(transformX(x), transformY(y), atomicFuncListB, funcListB, combinationListB);
                     imgBitmap.SetPixel(x, y, color
                        .WithRed((byte)(RGBvalues[0] * 255))
                        .WithGreen((byte)(RGBvalues[1] * 255))
